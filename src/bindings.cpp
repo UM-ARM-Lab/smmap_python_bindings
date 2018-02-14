2daf2042c623e0350fcad74ad7d06ae0b55bf05f
Dale McConachie <dmcconac@umich.edu>
2018-02-14 15:24:20 -0500

    Sample bindings for Eigen/function stuff.

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,6 +1,10 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/eigen.h>
+#include <pybind11/stl.h>
 
 #include <iostream>
+#include <functional>
 
 #include "smmap/planner.h"
 
@@ -29,12 +33,14 @@ private:
     // These need to be created in a seperate thread to avoid GIL problems, so use "lazy" initialization
     void execute_impl() const
     {
+        assert(ros::isInitialized());
+
         ros::NodeHandle nh;
         ros::NodeHandle ph("smmap_planner_node");
 
         ROS_INFO("Creating utility objects");
         smmap::RobotInterface robot(nh);
-        smmap_utilities::Visualizer vis(nh, ph);
+        smmap_utilities::Visualizer::Ptr vis = std::make_shared<smmap_utilities::Visualizer>(nh, ph);
         smmap::TaskSpecification::Ptr task_specification(smmap::TaskSpecification::MakeTaskSpecification(nh, ph, vis));
 
         ROS_INFO("Creating and executing planner");
@@ -45,6 +51,24 @@ private:
     }
 };
 
+//using MatrixXdR = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+//void SetJacobian(const Eigen::Ref<MatrixXdR>& jacobian)
+//void SetJacobian(const Eigen::Ref<Eigen::MatrixXd>& jacobian)
+void SetJacobian(const Eigen::MatrixXd& jacobian)
+{
+//    std::cout << "Ind: " << manipulator_ind << std::endl;
+    std::cout << "Jacobian:\n" << jacobian << std::endl;
+}
+
+//static std::function<Eigen::MatrixXd()> global_callback;
+
+void SetJacobianCallbackFunction(const std::function<Eigen::MatrixXd(int)>& callback)
+{
+    const Eigen::MatrixXd jacobian = callback(1);
+    std::cout << jacobian << std::endl;
+}
+
 PYBIND11_MODULE(smmap_python_bindings, m)
 {
     m.doc() = "SMMAP Library Plugin";
@@ -52,6 +76,13 @@ PYBIND11_MODULE(smmap_python_bindings, m)
     py::class_<SmmapWrapper>(m, "SmmapWrapper")
         .def(py::init<>())
         .def("execute", &SmmapWrapper::execute);
+
+    m.def("SetJacobian", &SetJacobian, "Sets the Jacobian for the indicted manipulator");
+
+    m.def("SetJacobianCallbackFunction", &SetJacobianCallbackFunction, "Function pointer test");
+
+//    m.def("cholesky1", [](Eigen::Ref<MatrixXdR> x) -> Eigen::MatrixXd { return x.llt().matrixL(); });
+//    m.def("cholesky2", [](const Eigen::Ref<const MatrixXdR> &x) -> Eigen::MatrixXd { return x.llt().matrixL(); });
 }
 
 
